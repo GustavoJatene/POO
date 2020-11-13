8514efe31b9baf61c89b1320255ee9f50146ad46
GustavoJatene <jateneg@gmail.com>
2020-11-13 10:03:39 -0300

    Classe Animais

---
+++ animais.py
@@ -0,0 +1,13 @@
+class Animais:
+    def __init__(self, nome, idade, peso, cor_dos_olhos):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.corOlhos = cor_dos_olhos
+
+    def comer(self):
+        pass
+
+    def dormir(self):
+        pass
+        
