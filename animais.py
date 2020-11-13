6fbcae0d9be740533de1f8408e1aa81ec9d73891
GustavoJatene <jateneg@gmail.com>
2020-11-13 19:03:49 -0300

    Classe Animais

--- animais.py
+++ animais.py
@@ -1,13 +1,27 @@
 class Animais:
-    def __init__(self, nome, idade, peso, cor_dos_olhos):
+
+    def __init__(self, nome, idade, peso, corOlhos, comendo=False, dormindo=False):
         self.nome = nome
         self.idade = idade
         self.peso = peso
-        self.corOlhos = cor_dos_olhos
+        self.corOlhos = corOlhos
+        self.comendo = comendo
+        self.dormindo = dormindo
 
-    def comer(self):
-        pass
+
+    def comer(self, alimento):
+        if self.comendo:
+            print(f'{self.nome} já está comendo')
+            return
+        print(f'O {self.nome} está comendo {alimento}.')
+        self.comendo = True
+
+    def p_comer(self):
+        if not self.comendo:
+            print(f'O {self.nome} continua sem comer.')
+            return
+        print(f'{self.nome} parou de comer')
+        self.comendo = False
 
     def dormir(self):
         pass
-        
