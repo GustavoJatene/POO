4d8efab043d938da2448dcdf84a0c80060cdc866
GustavoJatene <jateneg@gmail.com>
2020-11-16 14:14:01 -0300

    testando no main, os updates da classe animais

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from animais import Animais
 
-a1 = Animais('JACARE',0 ,0  , '')
+a1 = Animais('JACARE', 11, 235, '',)
 
-a1.comer('maçã')
-a1.comer('Carne')
-a1.p_comer()
-a1.p_comer()
+a1.comer('peixe')
+a1.comer('frango')
+a1.parar_comer()
+a1.parar_comer()
+a1.dormir()
+a1.dormir()
