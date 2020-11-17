72b9a0ff53caff485a9123d85abb327e5220391d
GustavoJatene <jateneg@gmail.com>
2020-11-17 16:01:59 -0300

    Visualizando status da herança

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from animais import Animais
+from mamiferos import Mamiferos
 
-a1 = Animais('JACARE', 11, 235, '',)
+a1 = Animais('JACARE', 11, 235, '')
+a2 = Mamiferos('JACARE2', 11, 235, '', '', 0)
 
 a1.comer('peixe')
 a1.comer('frango')
@@ -13,3 +15,5 @@ a1.parar_comer()
 a1.dormir()
 a1.dormir()
 a1.comer('pinha')
+a2.comer('peixe')
+a2.sentar()
