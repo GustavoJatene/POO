ca1b9a2be47012c5f547e63eed6a2d835e984ded
GustavoJatene <jateneg@gmail.com>
2020-11-17 16:01:38 -0300

    Implementando herança na classe mamiferos

--- mamiferos.py
+++ mamiferos.py
@@ -1,9 +1,15 @@
-import animais
-class Mamiferos:
-    def __init__(self, corPelos, comprimentoPelos, sentar = False, amamentar = False):
+from animais import Animais
+class Mamiferos(Animais):
+    def __init__(self, nome, idade, peso, corOlhos, corPelos, comprimentoPelos, sentado = False, amamentar = False):
+        Animais.__init__(self, nome, idade, peso, corOlhos)
         self.corPelos = corPelos
         self.comprimentoPelos = comprimentoPelos
-        self.sentar = sentar
+        self.sentado = sentado
         self.amamentar = amamentar
 
-    
+    def sentar(self):
+        if self.sentado:
+            print(f'{self.nome} Já está sentado')
+            return
+        print(f'{self.nome} Sentou')
+        self.sentado = True
