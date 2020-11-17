73ba310221280f8d647f2c3a81c50561b9d222e8
GustavoJatene <jateneg@gmail.com>
2020-11-16 22:05:16 -0300

    Atributos classe mamiferos

--- mamiferos.py
+++ mamiferos.py
@@ -1,1 +1,9 @@
 import animais
+class Mamiferos:
+    def __init__(self, corPelos, comprimentoPelos, sentar = False, amamentar = False):
+        self.corPelos = corPelos
+        self.comprimentoPelos = comprimentoPelos
+        self.sentar = sentar
+        self.amamentar = amamentar
+
+    
