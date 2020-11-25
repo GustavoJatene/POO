4b08aec7f88e40428a0ab2cb8ba75000421dc13f
GustavoJatene <jateneg@gmail.com>
2020-11-25 10:19:25 -0300

    Classe repteis

---
+++ repteis.py
@@ -0,0 +1,10 @@
+from animais import Animais
+class Aves(Animais):
+    def __init__(self, nome, idade, peso, corOlhos, tipoEscamas, comprimentoCalda, rastejando = False):
+        Animais.__init__(self, nome, idade, peso, corOlhos)
+        self.tipoEscamas = tipoEscamas
+        self.comprimentoCalda = comprimentoCalda
+        self.rastejand = rastejando
+
+    def rastejar(self):
+        pass
