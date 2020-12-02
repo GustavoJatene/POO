b51cbf7234a590a829df8d7780239ccf2ec1514a
GustavoJatene <jateneg@gmail.com>
2020-12-02 09:06:47 -0300

    Testanto classes

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from animais import Animais
 from mamiferos import Mamiferos
+from aves import Aves
+from repteis import Repteis
 
 a1 = Animais('JACARE', 11, 235, '')
 a2 = Mamiferos('Lemori', 11, 235, '', '',0)
+a3 = Aves('Pato', 2, 55, '', '', 0)
+a4 = Repteis('Lagarto', 5, 10, '', '', '', 20)
 
 a1.comer('peixe')
 a1.comer('frango')
@@ -19,3 +23,5 @@ a2.comer('peixe')
 a2.dormir()
 a2.parar_comer()
 a2.dormir()
+a3.dormir()
+a4.comer('Mosca')
